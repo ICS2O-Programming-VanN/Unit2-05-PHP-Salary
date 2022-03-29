03dde99eba58cceb010ec00a591890985c6b1502
Van Nguyen <99274250+van-nguyen2006@users.noreply.github.com>
2022-03-29 17:37:25 +0000

    Added color and more comments. Changed TAX_RATE to constant.

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
   </head>
   <body>
 		<!-- Heading and paragraph -->
-    <?php
+    <?php 
 			echo '<center><h1>Salary Program in PHP</h1></center><br>';
-			echo "<p>This website allows you to calculate your salary and the income tax you'll be paying the gouvernment! <br> Enter your hours worked and hourly rate below:</p>";
+			echo "<p>This website allows you to calculate your salary and the income tax you'll be paying the government! <br> Enter your hours worked and hourly rate below:</p>";
 		?>
 		<!-- Text field and button -->
 		<form method = "post">
@@ -21,18 +21,20 @@
 			<br><br>   
 			Rate $<input type="number" name="rate">/per hour<br>
 			<br><br>
-			<input type = "submit" name = "submit" value="Calculate/Pay">
+			<input type = "submit" name = "submit" value="Pay">
 		<!-- Calculations for salary and tax (With User Input) -->
 		<?php
-			if(isset($_POST['submit'])) {
-				$hours = $_POST['hours'];
+      define ("TAX_RATE", 0.1805); // constant
+			if(isset($_POST['submit'])) {	// if submit pressed
+        // Variables
+        $hours = $_POST['hours'];
 				$rate = $_POST['rate'];
-				$tax = 0.1805;
         $salary = $hours*$rate;
-				$salary = number_format($salary, 2);
-        $salarytaxed = $salary*$tax;
+				$salary = number_format($salary, 2);        
+        $salarytaxed = $salary*TAX_RATE;
         $salarytaxed = number_format($salarytaxed, 2);
-				echo "<br><br>Your salary is $" . $salary . " <br> The government will be taking $" . $salarytaxed . " as income tax.";   
+        // Output
+				echo "<br><br><h3>Your salary is $" . $salary . " <br> The government will be taking $" . $salarytaxed . " as income tax.</h3>";   
 			}
 		?>
   </body>
